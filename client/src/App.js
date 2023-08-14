d365328fbaee111b2c27ee65fa0cb543f59dc54b
Evan <evanknight18@gmail.com>
2023-08-14 08:29:55 -0600

    files expanded

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,55 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ChakraProvider, Flex } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Map from './components/Map';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import Profile from './components/Profile';
+import theme from './theme';
+
+const httpLink = createHttpLink({
+  url: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Map} />
-        <Route path="/login" component={LoginForm} />
-        <Route path="/register" component={RegisterForm} />
-        <Route path="/profile" component={Profile} />
-        {/* you might want to add more routes here */}
-      </Switch>
-    </Router>
+    <ChakraProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <Router>
+          {/* You can add a header component here if you have one */}
+          <Flex as="main" padding="1.5rem">
+            <Routes>
+              <Route path="/" element={<Map />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<RegisterForm />} />
+              <Route path="/profile" element={<Profile />} />
+              {/* Add more routes as needed */}
+            </Routes>
+          </Flex>
+          {/* You can add a footer component here if you have one */}
+        </Router>
+      </ApolloProvider>
+    </ChakraProvider>
   );
 }
 
 export default App;
+
